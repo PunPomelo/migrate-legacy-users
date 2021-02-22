722029fffccea91e5bb3d013a184fc91122fe273
PunPomelo <78335708+PunPomelo@users.noreply.github.com>
2021-02-22 18:54:14 +0700

    add lambdas

---
+++ migrateFirebaseUsers/index.js
@@ -0,0 +1,10 @@
+'use strict';
+const AWS = require('aws-sdk');
+
+exports.handler = function (event, context, callback) {
+  console.log('HelloWorld!!!!');
+  // console.log(JSON.stringify(`Event: event`));
+  // Lambda Code Here
+  // context.succeed('Success!')
+  // context.fail('Failed!')
+};

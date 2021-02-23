b8c81a6089f8523f10c194304349b440b27e7112
PunPomelo <78335708+PunPomelo@users.noreply.github.com>
2021-02-23 11:47:29 +0700

    finish trigger lambda from s3

--- lambda.tf
+++ lambda.tf
@@ -38,3 +38,24 @@ resource "aws_lambda_function" "migrateFirebaseUsers" {
 
   role = aws_iam_role.lambda_exec.arn
 }
+
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.migrateFirebaseUsers.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.firebase_users_bucket.arn
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.firebase_users_bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.migrateFirebaseUsers.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "AWSLogs/"
+    filter_suffix       = ".log"
+  }
+
+  depends_on = [aws_lambda_permission.allow_bucket]
+}
